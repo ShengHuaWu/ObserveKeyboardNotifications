08ce4491484f44cf3d1e6e5f26a63ebf8e9628ff
ShengHua Wu <fantasy0404@gmail.com>
2017-04-24 10:12:18 +0800

    Initiate project.

---
+++ ObserveKeyboardNotifications/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ObserveKeyboardNotifications
+//
+//  Created by ShengHua Wu on 24/04/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
